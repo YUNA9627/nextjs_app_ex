a37921952cb99cbea199f4f6117609e905172503
YUNA9627 <yuna2175@naver.com>
2024-11-14 10:54:07 +0900

    page.js 추가

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,11 @@
-export default function Read(props) {
+export default async function Read(props) {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL+`topics/${props.params.id}`);
+  const topic = await response.json();
   return (
     <div>
-      <h2>Read</h2>
-      <p>parameter: {props.params.id}</p>
+      <h2>{topic.title}</h2>
+      {/* <p>parameter: {props.params.id}</p> */}
+      <p>{topic.body}</p>
     </div>
   );
 }
