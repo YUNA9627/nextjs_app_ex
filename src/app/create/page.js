1de0a00d2297cd4e91e2cda45cefcedc04dc1fd9
YUNA9627 <yuna2175@naver.com>
2024-11-14 10:53:50 +0900

    page.js 추가

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,40 @@
+"use client";
+import { useRouter } from 'next/navigation'
+
 export default function Create() {
+  const router = useRouter()
+  
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title, body}) // 문자열로 바꿈 (object->json)
+    }
+    fetch(process.env.NEXT_PUBLIC_API_URL+'topics', options)
+      .then(res=>res.json()) // 결과를 객체로 변환
+      .then(result=>{
+        console.log(result)
+        router.push(`/read/${result.id}`)
+        router.refresh();
+      })
+  }
   return (
     <div>
-      <p>Create</p>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="text" name="title" placeholder="title"/>
+        </div>
+        <div>
+          <textarea name="body" placeholder="content"></textarea>
+        </div>
+        <button type="submit">전송</button>
+      </form>
+      <hr/>
     </div>
   );
 }
